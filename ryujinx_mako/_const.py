516276048e753e31c5320837f5f40f1d778bb37b
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-08-24 23:50:03 +0200

    Add constants to private module

---
+++ ryujinx_mako/_const.py
@@ -0,0 +1,31 @@
+import os
+try:
+    # Available with Python 3.11
+    from enum import StrEnum
+except ImportError:
+    from enum import Enum
+
+    class StrEnum(str, Enum):
+        pass
+
+
+class ConfigKey(StrEnum):
+    AppID = "MAKO_APP_ID"
+    PrivateKey = "MAKO_PRIVATE_KEY"
+    InstallationID = "MAKO_INSTALLATION_ID"
+
+
+NAME = "Ryujinx-Mako"
+SCRIPT_NAME = NAME.lower().replace("-", "_")
+
+# Check environment variables
+for key in ConfigKey:
+    if key not in os.environ.keys():
+        raise KeyError(f"Required environment variable not set: {key}")
+
+APP_ID = int(os.environ[ConfigKey.AppID])
+PRIVATE_KEY = os.environ[ConfigKey.PrivateKey]
+INSTALLATION_ID = int(os.environ[ConfigKey.InstallationID])
+
+GH_BOT_SUFFIX = "[bot]"
+GH_EMAIL_TEMPLATE = "{user_id}+{username}@users.noreply.github.com"
